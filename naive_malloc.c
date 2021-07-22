c62045071c7df76fbadb3e53598d1a2e9a89e86e
lh1008 <paintluis@protonmail.com>
2021-07-22 16:56:30 -0500

    Added naive_malloc.c file standard structure

---
+++ naive_malloc.c
@@ -0,0 +1,12 @@
+#include "malloc.h"
+
+/**
+ * naive_malloc - entry to naive malloc
+ * Desc: naive_malloc function that allocates
+ * memory that is suitable alignedfor any variable
+ * @size: size for memory to be allocated
+ * Return: pointer to the allocatedmemory
+ */
+void *naive_malloc(size_t size)
+{
+}

4b2c0d7aa723d74def8a020bc99b2bbb35767790
lh1008 <paintluis@protonmail.com>
2021-07-23 16:09:16 -0500

    Added free.c task 3

---
+++ free.c
@@ -0,0 +1,23 @@
+#include "malloc.h"
+
+/**
+ * _free - free memory block
+ * @ptr: pointer to a memory block
+*/
+void _free(void *ptr)
+{
+	char *p, *next;
+
+	if (!ptr)
+		return;
+	p = ptr;
+	p -= HDR_SZ;
+	next = p + GET_SIZE(p);
+	if (_GET_SIZE(next) & 1)
+	{
+		putchar('\n');
+		kill(getpid(), SIGABRT);
+	}
+	((block_info *)next)->prev = GET_SIZE(p);
+	((block_info *)next)->size |= 1;
+}
--- malloc.h
+++ malloc.h
@@ -52,5 +52,6 @@ typedef struct block_info_s
 void *naive_malloc(size_t size);
 void *_malloc(size_t size);
 void print_heap(void);
+void _free(void *ptr);
 
 #endif /* MALLOC_H */
